c647bc2cbcc0d2e503b216118f7e6ecc0c1bbbe7
Alexandre Acens <aacens@c2r6p2.42perpignan.fr>
2023-08-24 18:42:22 +0200

    modif

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,6 +1,20 @@
 #include <unistd.h>
 
-void ft_rev_int_tab(int *tab, int size)
+void ft_rev_int_tab(int *tab, int size) 
 {
-    int t;
+    int start; 
+    int end;
+    int stockend;
+
+    start = 0;
+    end = size - 1;
+
+    while (start < end) 
+    {
+        stockend = tab[start];
+        tab[start] = tab[end];
+        tab[end] = stockend;
+        start++;
+        end--;
+    }
 }
